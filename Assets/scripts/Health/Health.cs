a52fb64ed70092c0af439b01afa9764c97a46c8b
Lvanhuijstee <lennyhuijstee@hotmail.com>
2026-06-05 15:43:36 +0200

    added Iframes
    added iframes + hurt animations

--- Assets/scripts/Health/Health.cs
+++ Assets/scripts/Health/Health.cs
@@ -1,18 +1,26 @@
+using System.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")] 
     [SerializeField] private float startingHealth;
     public float currentHealth;
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField]private float iFrameDuration;
+    [SerializeField] private int counter;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -25,6 +33,7 @@ public class Health : MonoBehaviour
             //player hurt
             anim.SetTrigger("hurt");
             //iframes
+            StartCoroutine(invunerability());
         }
         else
         {
@@ -41,5 +50,16 @@ public class Health : MonoBehaviour
     public void addHealth(float _value) {
         currentHealth = Mathf.Clamp(currentHealth +_value, 0, startingHealth);
     }
-    
+
+    private IEnumerator invunerability() {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        // duration
+        for (int i = 0; i < counter; i++) {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration / (counter * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (counter * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+    }
 }
