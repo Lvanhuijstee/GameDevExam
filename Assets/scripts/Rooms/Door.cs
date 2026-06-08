616d6c139007967e8761bf295af9feba409fea1f
Lvanhuijstee <lennyhuijstee@hotmail.com>
2026-06-08 09:26:55 +0200

    added new traps

--- Assets/scripts/Rooms/Door.cs
+++ Assets/scripts/Rooms/Door.cs
@@ -6,16 +6,29 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform nextRoom;
     [SerializeField] private cameraController cam;
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "Player") {
+    private void Awake()
+    {
+        cam = Camera.main.GetComponent<cameraController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
             if (collision.transform.position.x < transform.position.x)
             {
                 cam.MoveToNewRoom(nextRoom);
+                previousRoom.GetComponent<Room>().ActivateRoom(false);
+                nextRoom.GetComponent<Room>().ActivateRoom(true);
+                
             }
-            else {
+            else
+            {
                 cam.MoveToNewRoom(previousRoom);
+                nextRoom.GetComponent<Room>().ActivateRoom(false);
+                previousRoom.GetComponent<Room>().ActivateRoom(true);
+                
             }
-
         }
-    } 
+    }
 }
